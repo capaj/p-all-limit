df814e04c818278f8dfb9a28bfb2ed0b749aa09d
Jiří Špác <capajj@gmail.com>
2023-01-03 14:21:09 +0100

    fix ordering of results

--- p-all-limit.ts
+++ p-all-limit.ts
@@ -7,19 +7,21 @@ export const promiseAllLimit = <T>(
 
   return new Promise((resolve, reject) => {
     let results = [] as T[]
+    let itemIndex = -1
     const next = () => {
+      itemIndex++
+
       if (array.length === 0 && inFlight === 0) {
         return resolve(results)
       }
 
       while (inFlight < limit && array.length > 0) {
         inFlight++
-
         const nextItem = array.shift() as T
 
         iterator(nextItem)
           .then((singleResult) => {
-            results.push(singleResult)
+            results[itemIndex] = singleResult
             inFlight--
             next()
           })
